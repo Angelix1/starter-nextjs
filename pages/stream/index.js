9b6a8f53ca7388ea26b5e01624b7e65292f5e92e
Angel <86276165+Angelix1@users.noreply.github.com>
2022-09-15 18:53:59 +0800

    Update index.js

--- pages/stream/index.js
+++ pages/stream/index.js
@@ -1,11 +1,14 @@
 import Script from 'next/script';
 import Stream from '../../components/stream';
 import SidePanel from '../../components/sidePanel';
+import Header from '../../components/header';
+    
 
 
 export default function Home() {
   
   return (<>
+    <Header title="Music Player" description="Using NextJs for dynamic website" icon="/favicon.ico"/>
     <Script type="text/javascript" src="/stream/script.js"></Script> 
     <Stream />
   
