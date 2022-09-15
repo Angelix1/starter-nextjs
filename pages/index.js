7cf5497ccf3cd4ef7715e5675253c2baf3efd0da
Angel <86276165+Angelix1@users.noreply.github.com>
2022-09-15 19:51:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 import Header from '../components/header';
 
 
 export default function Home() {
   return (<>
-    <Header title="Music Player" description="Using NextJs for dynamic website" icon="/favicon.ico"/>          
+    <Header title="Home" description="Using NextJs for dynamic website" icon="/favicon.ico"/>
     <div className={styles.container}>
+      <br></br>
       <main className={styles.main}>
-        <p className={styles.description}>
-          Try out streaming youtube video without Ads{' '}
-          <Link href="api/stream" passHref>
-            <code className={styles.code}>api/hello</code>
-          </Link>
-        </p>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://google.com">Angel's Website!</a>
+        </h1>
+        <br></br>
+        <div className={styles.grid}>
+          <a href="/stream" className={styles.card}>
+            <h2>Stream &rarr;</h2>
+            <h3>Stream Youtube Music without Ads for free.</h3>
+          </a>
+        </div>
       </main>
     </div>
   </>)
