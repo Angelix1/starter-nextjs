b42ac91a8b7a22d7be0c578b365ec14b46964356
Angel <86276165+Angelix1@users.noreply.github.com>
2022-09-15 16:13:04 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/sidebar.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+      <Component {...pageProps} />
+  );
 }
-
-export default MyApp
