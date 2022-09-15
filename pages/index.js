9b80ebbf3224a3e50e64e5d2c3fab7b0d5289a71
Angel <86276165+Angelix1@users.noreply.github.com>
2022-09-15 18:53:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import Header from '../../components/header';
+import Header from '../components/header';
 
 
 export default function Home() {
